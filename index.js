97b213c63125e3ff473556b59a8f33f9848f38e2
Ebe05 <ebenezerjason5@gmail.com>
2022-06-22 15:25:51 +0800

    Setup done
    views, method-override, connecting to mongodb, parsing formdata, models were setup in this commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const engine = require('ejs-mate');
+const path = require('path');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose')
+
+//connecting to mongo
+mongoose.connect('mongodb://localhost:27017/kkk');
+
+//setting up view engine
+app.set('view engine', 'ejs');
+app.engine('ejs', engine);
+app.set('views', path.join(__dirname, '/views'));
+
+//setting up method-override
+app.use(methodOverride('_method'))
+
+//parsing form data
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+app.get('/application', (req, res) => {
+    res.render("application");
+});
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});
